2b14cb87a74b2499a1dd75e350fedd786e850d6e
oarabilexolani <147558812+oarabilexolani@users.noreply.github.com>
2023-10-11 09:37:56 +0200

    Update Oarabile.sh

--- Oarabile.sh
+++ Oarabile.sh
@@ -4,7 +4,11 @@
 sudo apt-got update
 sudo apt-get -y install nginx
 systemctl start nginx
+echo "NGINX IS RUNNING"
+sleep 1
 
+if [[ systemctl is-active nginx ]]
+then
 mkdir /var/ww/html/labs
 chown $USER:root /var/www/html/labs
 
@@ -14,3 +18,9 @@ cd html
 touch index.html
 
 echo "<html><body><h1>HELLO</h1></body></html>" >> index.html
+
+else
+echo "error"
+exit 1 
+
+fi
